efcc133f475ff3fabee3f1d867bb1a685ca80cb0
Aamir Sohail <112603549+learningtechcoding@users.noreply.github.com>
2025-04-09 21:47:40 +0500

    final

--- js/main.js
+++ js/main.js
@@ -13,45 +13,7 @@ $(function() {
 
   "use strict";
   /***************************
-
-  preloader
-
-  ***************************/
-  $(document).ready(function() {
-    $(".sb-loading").animate({
-      opacity: 1
-    }, {
-      duration: 500,
-    });
-    setTimeout(function() {
-      $('.sb-preloader-number').each(function() {
-        var $this = $(this),
-          countTo = $this.attr('data-count');
-        $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        }, {
-          duration: 1000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-        });
-      });
-      $(".sb-bar").animate({
-        height: '100%'
-      }, {
-        duration: 1000,
-        complete: function() {
-
-          $(".sb-preloader").addClass('sb-hidden')
-        }
-      });
-    }, 400);
-  });
-  /***************************
-
+ 
   faq
 
   ***************************/
@@ -410,20 +372,7 @@ $(function() {
   REINIT
 
   ------------------------------------------------------------
-  ----------------------------------------------------------*/
-  document.addEventListener("swup:contentReplaced", function() {
-    $('.sb-info-btn , .sb-info-bar , .sb-minicart , .sb-menu-btn , .sb-navigation').removeClass('sb-active');
-    $('.sb-top-bar-frame').removeClass('sb-scroll');
-    $('a').removeClass('sb-click');
-    if ($('html').hasClass('is-rendering')) {
-      $("html, body").animate({
-        scrollTop: 0
-      }, {
-        duration: 0,
-        complete: function() {}
-      });
-    }
-    /***************************
+ ********************
 
     faq
 
